7cd7f34a0f0cafe07000b45630879d0a76412c3f
xjy <2642630566@qq.com>
2026-02-24 17:10:27 +0800

    refactor: remove legacy shujia codebase and old-box leftovers

--- src/main/java/com/xjy/shujia/mapper/DictMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xjy.shujia.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xjy.shujia.entity.Dict;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface DictMapper extends BaseMapper<Dict> {
-}
--- src/main/java/com/xjy/shujia/mapper/UserMapper.java
+++
@@ -1,9 +0,0 @@
-package com.xjy.shujia.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.xjy.shujia.entity.User;
-import org.apache.ibatis.annotations.*;
-
-@Mapper
-public interface UserMapper extends BaseMapper<User> {
-}
